e3613092b57a7a6de8098b20eb71ec787f72a895
Chong-U Lim <me@chongdashu.com>
2024-09-13 11:36:19 +0100

    Update images

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,37 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Confetti Coder - Add a Sprinkle of Joy to Your Coding",
   description: "Confetti Coder is a delightful VS Code extension that adds customizable confetti effects to your coding sessions, making development more enjoyable.",
+  metadataBase: new URL('https://confetticoder.xyz'),
+  openGraph: {
+    title: 'Confetti Coder',
+    description: 'Add a Sprinkle of Joy to Your Coding',
+    url: 'https://confetticoder.xyz',
+    siteName: 'Confetti Coder',
+    images: [
+      {
+        url: 'https://confetticoder.xyz/og-image.png', // Make sure to create this image
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Confetti Coder',
+    description: 'Add a Sprinkle of Joy to Your Coding',
+    images: ['https://confetticoder.xyz/og-image.png'], // Same as OpenGraph image
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest', 
 };
 
 export default function RootLayout({
