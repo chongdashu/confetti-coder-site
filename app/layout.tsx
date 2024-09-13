cbdb823717e52323b2875654e8d27f4a1c595205
Chong-U Lim <me@chongdashu.com>
2024-09-13 13:21:29 +0100

    posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { CSPostHogProvider } from "./providers";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -57,11 +57,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <CSPostHogProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      </CSPostHogProvider>
     </html>
   );
 }
